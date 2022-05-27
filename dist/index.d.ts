b56c6f1881e3de50f337afbde678053f8b9e1e16
hieyou1 <31020711+hieyou1@users.noreply.github.com>
2022-05-26 19:56:06 -0500

    add a declaration too because otherwise vscode :(

---
+++ dist/index.d.ts
@@ -0,0 +1,35 @@
+declare type SigMethod = 'HMAC-SHA1' | 'PLAINTEXT';
+declare type FullToken = {
+    oauth_consumer_key: string;
+    oauth_nonce: string;
+    oauth_signature_method: SigMethod;
+    oauth_timestamp: number;
+    oauth_token: string;
+    oauth_version: '1.0';
+};
+declare type Token = {
+    oauth_token: string;
+    oauth_token_secret: string;
+};
+export declare class SchoologyAPI {
+    client_key: string;
+    client_secret: string;
+    site_base: string;
+    api_base: string;
+    oauth_token: string;
+    oauth_token_secret: string;
+    redirectLoop: boolean;
+    fetch: typeof fetch;
+    constructor(client_key: string, client_secret: string, site_base?: string, api_host?: string);
+    getAuthHeaderComponents(signatureMethod?: SigMethod, token?: string): FullToken;
+    getUnsignedAuthHeader(): string;
+    getPlaintextAuthHeader(): string;
+    getSignedAuthHeader(method: string, url: string): string;
+    setToken(token: Token): void;
+    getRequestToken(): Promise<Token>;
+    getConnectURL(returnUrl: string): string;
+    getAccessToken(requestToken: any): Promise<string>;
+    easyFetch(url: RequestInfo, init?: {}): Promise<any>;
+}
+export {};
+//# sourceMappingURL=index.d.ts.map
