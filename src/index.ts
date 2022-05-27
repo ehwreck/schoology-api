471eceb71782e68f34fd0ba99795cac83e8b2f83
hieyou1 <31020711+hieyou1@users.noreply.github.com>
2022-05-26 19:47:53 -0500

    TS Time

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,52 @@
-const { v4 } = require("uuid");
-const { isBrowser: IS_BROWSER } = require("is-in-browser");
-const qsParse = (search) => {
+import { v4 } from "uuid";
+import { isBrowser as IS_BROWSER } from "is-in-browser";
+const qsParse = (search: string): any => {
     return Object.fromEntries(new URLSearchParams(search).entries());
 };
 const DEFAULT_SITE_BASE = 'https://www.schoology.com';
 const SCHOOLOGY_API_HOST = 'https://api.schoology.com';
 const SCHOOLOGYTEST_API_HOST = 'https://api.schoologytest.com';
 const REALM_PARAM = { 'OAuth realm': 'Schoology API' };
-const headerFormat = (components) => {
-    const parts = [];
-    Object.keys(components).forEach(key => parts.push(key + '="' + components[key] + '"'));
-    return parts.join(',');
+
+type SigMethod = 'HMAC-SHA1' | 'PLAINTEXT';
+
+type FullToken = {
+    oauth_consumer_key: string,
+    oauth_nonce: string,
+    oauth_signature_method: SigMethod,
+    oauth_timestamp: number,
+    oauth_token: string,
+    oauth_version: '1.0',
 };
-const baseStringFormat = (components) => {
-    const parts = [];
-    Object.keys(components).forEach(key => parts.push(key + '=' + components[key]));
-    return parts.join('&');
+
+type Token = {
+    oauth_token: string;
+    oauth_token_secret: string;
 };
-class SchoologyAPI {
-    constructor(client_key, client_secret, site_base = DEFAULT_SITE_BASE, api_host = null) {
+
+const headerFormat = (components: Record<string, string | number>) => {
+    const parts: string[] = []
+    Object.keys(components).forEach(key => parts.push(key + '="' + components[key] + '"'))
+    return parts.join(',')
+};
+
+const baseStringFormat = (components: Record<string, string | number>) => {
+    const parts: string[] = []
+    Object.keys(components).forEach(key => parts.push(key + '=' + components[key]))
+    return parts.join('&')
+};
+
+export class SchoologyAPI {
+    client_key: string
+    client_secret: string
+    site_base: string
+    api_base: string
+    oauth_token: string
+    oauth_token_secret: string
+    redirectLoop: boolean;
+    fetch: typeof fetch;
+
+    constructor(client_key: string, client_secret: string, site_base = DEFAULT_SITE_BASE, api_host: string = null) {
         if (IS_BROWSER) {
             this.fetch = window.fetch;
         } else {
@@ -30,7 +58,8 @@ class SchoologyAPI {
         this.api_base = `${(api_host ?? ((site_base.indexOf('schoologytest') !== -1) ? SCHOOLOGYTEST_API_HOST : SCHOOLOGY_API_HOST))}/v1`;
         this.redirectLoop = IS_BROWSER;
     }
-    getAuthHeaderComponents(signatureMethod = 'PLAINTEXT', token = '') {
+
+    getAuthHeaderComponents(signatureMethod: SigMethod = 'PLAINTEXT', token = ''): FullToken {
         const nonce = v4();
         const timestamp = Math.round(Date.now() / 1000);
         return {
@@ -42,14 +71,16 @@ class SchoologyAPI {
             oauth_version: '1.0',
         };
     }
-    getUnsignedAuthHeader() {
+
+    getUnsignedAuthHeader(): string {
         return headerFormat({
             ...REALM_PARAM,
             ...this.getAuthHeaderComponents(),
             oauth_signature: this.client_secret + '%26'
         });
     }
-    getPlaintextAuthHeader() {
+
+    getPlaintextAuthHeader(): string {
         const authHeaderComponents = this.getAuthHeaderComponents('PLAINTEXT', this.oauth_token);
         const key = [this.client_secret, this.oauth_token_secret].join('&');
         return headerFormat({
@@ -58,24 +89,27 @@ class SchoologyAPI {
             oauth_signature: key
         });
     }
-    getSignedAuthHeader(method, url) {
+
+    getSignedAuthHeader(method: string, url: string): string {
         const authHeaderComponents = this.getAuthHeaderComponents('HMAC-SHA1', this.oauth_token);
         const baseString = [method.toUpperCase(), url, baseStringFormat(authHeaderComponents)]
             .map(encodeURIComponent)
             .join('&');
         const key = [this.client_secret, this.oauth_token_secret].join('&');
-        const signature = ((isBrowser) ? require("create-hmac") : require("crypto").createHmac)('sha1', key).update(baseString).digest('base64');
+        const signature = ((IS_BROWSER) ? require("create-hmac") : require("crypto").createHmac)('sha1', key).update(baseString).digest('base64');
         return headerFormat({
             ...REALM_PARAM,
             ...authHeaderComponents,
             oauth_signature: signature
         });
     }
-    setToken(token) {
+
+    setToken(token: Token) {
         this.oauth_token = token.oauth_token;
         this.oauth_token_secret = token.oauth_token_secret;
     }
-    async getRequestToken() {
+
+    async getRequestToken(): Promise<Token> {
         const Authorization = this.getUnsignedAuthHeader();
         let res = await (await this.fetch(this.api_base + '/oauth/request_token', {
             headers: { Authorization }
@@ -84,10 +118,12 @@ class SchoologyAPI {
         this.setToken(token);
         return token;
     }
-    getConnectURL(returnUrl) {
+
+    getConnectURL(returnUrl: string): string {
         return `${this.site_base}/oauth/authorize?oauth_token=${this.oauth_token}&return_url=${returnUrl}`;
     }
-    async getAccessToken(requestToken) {
+
+    async getAccessToken(requestToken: any): Promise<string> {
         this.setToken(requestToken);
         let res = await (await this.fetch(this.api_base + '/oauth/access_token', {
             headers: {
@@ -98,7 +134,8 @@ class SchoologyAPI {
         this.setToken(token);
         return token;
     }
-    async easyFetch(url, init = {}) {
+
+    async easyFetch(url: RequestInfo, init = {}) {
         const that = this;
         const { fetch, getPlaintextAuthHeader } = that;
         if (!that.redirectLoop) {
@@ -109,7 +146,7 @@ class SchoologyAPI {
                 },
                 "redirect": "manual"
             });
-            let checkRes = async () => {
+            let checkRes = async (): Promise<any> => {
                 if (res.status.toString().indexOf("30") == 0 && res.headers.has("location")) {
                     res = await fetch(res.headers.get("location"), {
                         ...init,
@@ -126,7 +163,7 @@ class SchoologyAPI {
             return checkRes();
         } else {
             let res;
-            let tryFetch = async (cUrl) => {
+            let tryFetch = async (cUrl: RequestInfo): Promise<any> => {
                 res = await fetch(cUrl, {
                     ...init,
                     "headers": {
@@ -144,4 +181,3 @@ class SchoologyAPI {
         }
     }
 }
-module.exports = { SchoologyAPI };
